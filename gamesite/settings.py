aa0f43130d02d017f64ae85d872f086d8c9cacf8
Lukash <89802614+AlexanderLukash@users.noreply.github.com>
2022-07-28 19:24:33 +0300

    Edit settings.py

--- gamesite/settings.py
+++ gamesite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v!45v5xahx%)z*%0mi#j%19&&q-aub_2egdt%$wgb^^0ysfv8o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
